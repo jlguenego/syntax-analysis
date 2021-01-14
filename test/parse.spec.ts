0f80c40cd7ea7ee372bb0c943422ec95e94790b5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-14 19:31:54 +0100

    test: added LL0

--- test/parse.spec.ts
+++ test/parse.spec.ts
@@ -19,4 +19,8 @@ describe('BFS Unit Test', () => {
     const parseTree = parse<NTA, TA>(sentence, cfg, {method: 'DFS1'});
     assert.deepStrictEqual(parseTree, expectedParseTree);
   });
+  it('test parse with LL0', () => {
+    const parseTree = parse<NTA, TA>(sentence, cfg, {method: 'LL0'});
+    assert.deepStrictEqual(parseTree, expectedParseTree);
+  });
 });
