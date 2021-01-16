4f0725f8f9a8a785f5e9083ea15f0929fe4b019b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-16 12:13:13 +0100

    doc

--- src/terminals/epsilonTerminal.ts
+++ src/terminals/epsilonTerminal.ts
@@ -3,7 +3,7 @@ import {Terminal} from '../interfaces/Terminal';
 /**
  * Some CFG has ε-production (rules with RHS empty).
  * when this occurs we may need a special terminal
- * symbol called epsilon to indicate it.
+ * symbol called epsilon, or null symbol to indicate it.
  *
  * slide 283
  * https://web.stanford.edu/class/archive/cs/cs143/cs143.1128/lectures/03/Slides03.pdf
