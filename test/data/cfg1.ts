5bede07d53e40b65ff10714408c669c544a0d083
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-06 13:53:58 +0100

    dev: updated cfg 1 to get the same as cs143

--- test/data/cfg1.ts
+++ test/data/cfg1.ts
@@ -8,56 +8,37 @@ import {
   defineTerminalAlphabet,
 } from '../../src';
 
-const t = defineTerminalAlphabet(['+', 'int'] as const);
-const nt = defineNonTerminalAlphabet(['S', 'E', 'F'] as const);
+const t = defineTerminalAlphabet(['+', 'int', '(', ')'] as const);
+const nt = defineNonTerminalAlphabet(['E', 'T'] as const);
 
 const spec: CFGSpecifications<typeof t, typeof nt> = {
-  startSymbol: 'S',
+  startSymbol: 'E',
   productions: [
-    {LHS: 'S', RHS: ['E']},
-    {LHS: 'E', RHS: ['F']},
-    {LHS: 'E', RHS: ['E', '+', 'F']},
-    {LHS: 'F', RHS: ['int']},
+    {LHS: 'E', RHS: ['T']},
+    {LHS: 'E', RHS: ['T', '+', 'E']},
+    {LHS: 'T', RHS: ['int']},
+    {LHS: 'T', RHS: ['(', 'E', ')']},
   ],
 };
 export const cfg1 = new ContextFreeGrammar(spec as CFGSpec, t, nt);
 
-export const sentence: Sentence = ['int', '+', 'int', '+', 'int'].map(str => ({
+export const sentence: Sentence = ['int', '+', 'int'].map(str => ({
   name: str,
 }));
 
 export const expectedParseTree: ParseTree = {
-  node: nt.S,
+  node: nt.E,
   children: [
+    {
+      node: nt.T,
+      children: [{node: {name: 'int'}}],
+    },
+    {node: {name: '+'}},
     {
       node: nt.E,
       children: [
         {
-          node: nt.E,
-          children: [
-            {
-              node: nt.E,
-              children: [
-                {
-                  node: nt.F,
-                  children: [{node: {name: 'int'}}],
-                },
-              ],
-            },
-            {
-              node: {name: '+'},
-            },
-            {
-              node: nt.F,
-              children: [{node: {name: 'int'}}],
-            },
-          ],
-        },
-        {
-          node: {name: '+'},
-        },
-        {
-          node: nt.F,
+          node: nt.T,
           children: [{node: {name: 'int'}}],
         },
       ],
