3b6cf2360215c5c6f5ed288b183298e3232c9f96
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-09 14:13:51 +0100

    feat: added dfs2 async

--- src/top-down/DFS2.ts
+++ src/top-down/DFS2.ts
@@ -7,52 +7,56 @@ import {NonTerminal} from '../NonTerminal';
 import {PartialParseTree} from '../PartialParseTree';
 import {testFn} from './common';
 
+export const dfs2GetChildren = (
+  sentence: Sentence,
+  cfg: ContextFreeGrammar
+) => (ppt: PartialParseTree): PartialParseTree[] => {
+  // check
+  if (
+    !cfg.hasEmptyProduction() &&
+    ppt.sententialForm.symbols.length > sentence.length
+  ) {
+    return [];
+  }
+
+  // only the first non terminal needs to be yielded. (left most derivation)
+  const fnt = ppt.getFirstNonTerminal();
+  if (!fnt) {
+    return [];
+  }
+  const nts = fnt.subtree.node;
+
+  // CS143 slide
+  // the order of productions is now important. We take the one by looking at one lookahead token.
+  const lookAheadToken = ppt.getLookAheadToken(sentence);
+  const terminals = cfg.first(nts);
+  if (!terminals.map(t => t.name).includes(lookAheadToken.name)) {
+    return [];
+  }
+
+  const result = [];
+  const productions = cfg.productions
+    .filter(p => p.LHS === nts)
+    .filter(
+      p =>
+        p.RHS.symbols[0] instanceof NonTerminal ||
+        p.RHS.symbols[0].name === lookAheadToken.name
+    );
+  for (const prod of productions) {
+    result.push(ppt.yield(fnt.path, prod));
+  }
+
+  return result;
+};
+
 export const parseWithDFS2 = (
   sentence: Sentence,
   cfg: ContextFreeGrammar
 ): ParseTree => {
-  const getChildren = (ppt: PartialParseTree): PartialParseTree[] => {
-    // check
-    if (
-      !cfg.hasEmptyProduction() &&
-      ppt.sententialForm.symbols.length > sentence.length
-    ) {
-      return [];
-    }
-
-    // only the first non terminal needs to be yielded. (left most derivation)
-    const fnt = ppt.getFirstNonTerminal();
-    if (!fnt) {
-      return [];
-    }
-    const nts = fnt.subtree.node;
-
-    // CS143 slide
-    // the order of productions is now important. We take the one by looking at one lookahead token.
-    const lookAheadToken = ppt.getLookAheadToken(sentence);
-    const terminals = cfg.first(nts);
-    if (!terminals.map(t => t.name).includes(lookAheadToken.name)) {
-      return [];
-    }
-
-    const result = [];
-    const productions = cfg.productions
-      .filter(p => p.LHS === nts)
-      .filter(
-        p =>
-          p.RHS.symbols[0] instanceof NonTerminal ||
-          p.RHS.symbols[0].name === lookAheadToken.name
-      );
-    for (const prod of productions) {
-      result.push(ppt.yield(fnt.path, prod));
-    }
-
-    return result;
-  };
   const dfsTree = new DFSTree<PartialParseTree>(
     new PartialParseTree(new Tree<ParseSymbol>(cfg.startSymbol)),
     testFn(sentence),
-    getChildren
+    dfs2GetChildren(sentence, cfg)
   );
   const pt = dfsTree.search();
   if (pt === undefined) {
