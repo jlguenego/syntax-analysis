ed59bc1e7a770b088c4b4481ece5aae215fa37c8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-09 17:13:20 +0100

    feat: export ll1GetChildren

--- src/top-down/LL1.ts
+++ src/top-down/LL1.ts
@@ -7,40 +7,43 @@ import {ParseError} from '../ParseError';
 import {PartialParseTree} from '../PartialParseTree';
 import {testFn} from './common';
 
+export const ll1GetChildren = (sentence: Sentence, cfg: ContextFreeGrammar) => (
+  ppt: PartialParseTree
+): PartialParseTree[] => {
+  // only the first non terminal needs to be yielded. (left most derivation)
+  const nts = ppt.sententialForm.findFirstNonTerminal();
+  if (nts === undefined) {
+    const length = ppt.sententialForm.symbols.length;
+    if (length < sentence.length) {
+      throw new ParseError('LL(1) Parser: Syntax Error.', sentence[length]);
+    }
+    return [];
+  }
+
+  // CS143 slide
+  // the order of productions is now important. We take the one by looking at one lookahead token.
+  const lookAheadToken = ppt.getLookAheadToken(sentence);
+  const rhs = cfg.getfromLL1Table(nts, lookAheadToken);
+
+  const result = [];
+  const child = ppt.tree.clone();
+  const ntl = child.find(t => t.node === nts) as Tree<ParseSymbol>;
+  for (const s of rhs.symbols) {
+    child.graft(ntl, new Tree<ParseSymbol>(s));
+  }
+  result.push(new PartialParseTree(child));
+
+  return result;
+};
+
 export const parseWithLL1 = (
   sentence: Sentence,
   cfg: ContextFreeGrammar
 ): ParseTree => {
-  const getChildren = (ppt: PartialParseTree): PartialParseTree[] => {
-    // only the first non terminal needs to be yielded. (left most derivation)
-    const nts = ppt.sententialForm.findFirstNonTerminal();
-    if (nts === undefined) {
-      const length = ppt.sententialForm.symbols.length;
-      if (length < sentence.length) {
-        throw new ParseError('LL(1) Parser: Syntax Error.', sentence[length]);
-      }
-      return [];
-    }
-
-    // CS143 slide
-    // the order of productions is now important. We take the one by looking at one lookahead token.
-    const lookAheadToken = ppt.getLookAheadToken(sentence);
-    const rhs = cfg.getfromLL1Table(nts, lookAheadToken);
-
-    const result = [];
-    const child = ppt.tree.clone();
-    const ntl = child.find(t => t.node === nts) as Tree<ParseSymbol>;
-    for (const s of rhs.symbols) {
-      child.graft(ntl, new Tree<ParseSymbol>(s));
-    }
-    result.push(new PartialParseTree(child));
-
-    return result;
-  };
   const dfsTree = new DFSTree<PartialParseTree>(
     new PartialParseTree(new Tree<ParseSymbol>(cfg.startSymbol)),
     testFn(sentence),
-    getChildren
+    ll1GetChildren(sentence, cfg)
   );
   const pt = dfsTree.search();
   if (pt === undefined) {
