1c5ead7d58148abfc19e35f13041919ec39bffe4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-11 16:56:35 +0100

    test: added ex 5.2 of aho ullman

---
+++ test/data/aho_ullman/cfg5.2 copy.ts
@@ -0,0 +1,33 @@
+import {
+  CFGSpecifications,
+  ContextFreeGrammar,
+  Sentence,
+  ParseTree,
+  CFGSpec,
+  defineNonTerminalAlphabet,
+  defineTerminalAlphabet,
+} from '../../../src';
+
+const t = defineTerminalAlphabet(['a', 'b'] as const);
+const nt = defineNonTerminalAlphabet(['S', 'A'] as const);
+
+// this grammar is ???
+const spec: CFGSpecifications<typeof t, typeof nt> = {
+  startSymbol: 'S',
+  productions: [
+    {LHS: 'S', RHS: []},
+    {LHS: 'S', RHS: ['a', 'b', 'A']},
+    {LHS: 'A', RHS: ['S', 'a', 'a']},
+    {LHS: 'A', RHS: ['b']},
+  ],
+};
+export const cfg53 = new ContextFreeGrammar(spec as CFGSpec, t, nt);
+
+export const sentence53: Sentence = 'abaa'.split('').map(str => ({
+  name: str,
+}));
+
+export const expectedParseTree53: ParseTree = {
+  node: nt.S,
+  children: [],
+};
